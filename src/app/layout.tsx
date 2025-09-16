09340a78198c6175a914d5fa776dcac622c03001
Michael Mathias <mickmath86@gmail.com>
2025-09-16 16:00:22 -0700

    gtm tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { SanityLive } from '@/sanity/live'
 import { revalidateSyncTags } from '@/sanity/revalidateSyncTags'
 import '@/styles/tailwind.css'
 import type { Metadata } from 'next'
+import { GoogleTagManager } from '@next/third-parties/google' 
 
 export const metadata: Metadata = {
   title: {
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      
       <head>
+      <GoogleTagManager gtmId="GTM-WTJD5VKJ" />
         <link
           rel="stylesheet"
           href="https://api.fontshare.com/css?f%5B%5D=switzer@400,500,600,700&amp;display=swap"
@@ -30,6 +33,10 @@ export default function RootLayout({
         />
       </head>
       <body className="text-gray-950 antialiased">
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WTJD5VKJ"
+      height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+
         {children}
         <SanityLive revalidateSyncTags={revalidateSyncTags} />
       </body>
